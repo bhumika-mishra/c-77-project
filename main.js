abb2155c663b0cfd1e9f4d1860a9613cbd4d84f2
bhumika-mishra <96329652+bhumika-mishra@users.noreply.github.com>
2022-08-09 22:01:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name= "Ranbir";
+var last_name= "Kapoor";
+var a=10;
+var b=5;
+
+var first_name= "Ranbir";
+console.log=(first_name)
+var last_name= "Kapoor";
+console.log=(last_name)
+
+var add=a+b;
+console.log=(add);
